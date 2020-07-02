afec7b64bae129889c9276df800feeaec3cf1457
Priyadarshan Giri <giripriyadarshan@gmail.com>
2020-07-02 20:08:51 +0530

    code which prints reciprocal of letters and numbers

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
+use std::io;
 
+fn reciprocal_char(c : char) -> char {
+  match c {
+    'a'..='z' => (25 - (c as u8 - 'a' as u8) + 'a' as u8) as char,
+    'A'..='Z' => (25 - (c as u8 - 'A' as u8) + 'A' as u8) as char,
+    '0'..='9' => (9 - (c as u8 - '0' as u8) + '0' as u8) as char,
+    _ => c,   // Here we don't do anything
+  }
+}
+
+fn main() {
+  let mut input = String::new();
+  let stdin = io::stdin();
+  println!("Enter the string: ");
+  stdin.read_line(&mut input).expect("Error reading line!");
+  let reciprocated : String = input.chars().map(reciprocal_char).collect();
+  println!("Here's your string: {}", reciprocated);
+}
